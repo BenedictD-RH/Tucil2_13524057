831a12c9d8b63c10d7b872d0fad80f06c8c910da
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-03-21 19:50:17 +0700

    feat: implemented matrix package for rotation purposes

---
+++ src/matrix/matrix.go
@@ -0,0 +1,99 @@
+package matrix
+
+import "fmt"
+
+type Matrix struct {
+	Buffer [][]float32
+	rows   int
+	cols   int
+}
+
+func NewMatrix(row, col int) *Matrix {
+	mat := &Matrix{[][]float32{}, row, col}
+
+	for i := 0; i < row; i++ {
+		rowbuff := []float32{}
+		for j := 0; j < col; j++ {
+			rowbuff = append(rowbuff, 0)
+		}
+		mat.Buffer = append(mat.Buffer, rowbuff)
+	}
+	mat.rows = row
+	mat.cols = col
+	return mat
+}
+
+func AddMatrix(m1, m2 *Matrix) *Matrix {
+	if m1.rows != m2.rows || m1.cols != m2.cols {
+		return NewMatrix(0, 0)
+	} else {
+		m3 := NewMatrix(m1.rows, m2.cols)
+		for i := 0; i < m1.rows; i++ {
+			for j := 0; j < m2.cols; j++ {
+				m3.Buffer[i][j] = m1.Buffer[i][j] + m2.Buffer[i][j]
+			}
+		}
+		return m3
+	}
+}
+
+func SubtractMatrix(m1, m2 *Matrix) *Matrix {
+	if m1.rows != m2.rows || m1.cols != m2.cols {
+		return NewMatrix(0, 0)
+	} else {
+		m3 := NewMatrix(m1.rows, m2.cols)
+		for i := 0; i < m1.rows; i++ {
+			for j := 0; j < m2.cols; j++ {
+				m3.Buffer[i][j] = m1.Buffer[i][j] - m2.Buffer[i][j]
+			}
+		}
+		return m3
+	}
+}
+
+func MultiplyMatrix(m1, m2 *Matrix) *Matrix {
+	if m1.cols != m2.rows {
+		fmt.Println(m1.cols, "!=", m2.rows)
+		return NewMatrix(0, 0)
+	} else {
+		m3 := NewMatrix(m1.rows, m2.cols)
+		for i := 0; i < m1.rows; i++ {
+			for j := 0; j < m2.cols; j++ {
+				for k := 0; k < m1.cols; k++ {
+					m3.Buffer[i][j] += m1.Buffer[i][k] * m2.Buffer[k][j]
+				}
+			}
+		}
+		return m3
+	}
+}
+
+func Transpose(m *Matrix) (*Matrix){
+	mNew := NewMatrix(m.cols, m.rows)
+	for i := 0; i < mNew.rows; i++ {
+		for j := 0; j < mNew.cols; j++ {
+			mNew.Buffer[i][j] = m.Buffer[j][i]
+		}
+	}
+	return mNew
+}
+
+func Identity(n int) (*Matrix) {
+	m := NewMatrix(n,n)
+	for i:= 0; i < n; i++ {
+		m.Buffer[i][i] = 1
+	}
+	return m
+}
+
+func (m Matrix) String() string {
+	s := ""
+	for i := 0; i < m.rows; i++ {
+		s += "| "
+		for j := 0; j < m.cols; j++ {
+			s += fmt.Sprintf("%f ", m.Buffer[i][j])
+		}
+		s += "|\n"
+	}
+	return s
+}
---
+++ src/matrix/rotation.go
@@ -0,0 +1,37 @@
+package matrix
+
+import "math"
+
+func DegreeToRad(deg float32) (float32) {
+	return math.Pi*(deg/180)
+}
+
+func RotationMatrix_X(rad float32) (*Matrix) {
+	m := NewMatrix(3,3)
+	m.Buffer[0][0] = 1
+	m.Buffer[1][1] = float32(math.Cos(float64(rad)))
+	m.Buffer[1][2] = float32(math.Sin(float64(rad)))*-1
+	m.Buffer[2][1] = float32(math.Sin(float64(rad)))
+	m.Buffer[2][2] = float32(math.Cos(float64(rad)))
+	return m
+}
+
+func RotationMatrix_Y(rad float32) (*Matrix) {
+	m := NewMatrix(3,3)
+	m.Buffer[0][0] = float32(math.Cos(float64(rad)))
+	m.Buffer[0][2] = float32(math.Sin(float64(rad)))
+	m.Buffer[1][1] = 1
+	m.Buffer[2][0] = float32(math.Sin(float64(rad)))*-1
+	m.Buffer[2][2] = float32(math.Cos(float64(rad)))
+	return m
+}
+
+func RotationMatrix_Z(rad float32) (*Matrix) {
+	m := NewMatrix(3,3)
+	m.Buffer[0][0] = float32(math.Cos(float64(rad)))
+	m.Buffer[0][1] = float32(math.Sin(float64(rad)))*-1
+	m.Buffer[1][0] = float32(math.Sin(float64(rad)))
+	m.Buffer[1][1] = float32(math.Cos(float64(rad)))
+	m.Buffer[2][2] = 1
+	return m
+}
