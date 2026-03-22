538826743653a9d7c873a475ecc5e9362bd5fa2b
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-03-22 21:00:21 +0700

    feat: rough 3d rotation using drag

--- src/main.go
+++ src/main.go
@@ -1,30 +1,65 @@
 package main
 
 import (
-	// "fmt"
-	"go_project/object"
+	//"fmt"
+	"math"
 	"go_project/matrix"
+	"go_project/object"
+	//"image/color"
+
 	// "image/color"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	//"fyne.io/fyne/v2/canvas"
 
-	// "fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
+var	r_Mat *matrix.Matrix = matrix.RotationMatrix_X(matrix.DegreeToRad(180))
+var O *object.Object = object.ParseObject("../data/cow.obj")
+var view_3d *fyne.Container = container.NewWithoutLayout()
+type draggableRect struct {
+	*widget.Label
+}
+
+func (d *draggableRect) Dragged(ev *fyne.DragEvent) {
+    var dx, dy = float64(ev.Dragged.DX/600)*-1, float64(ev.Dragged.DY/600)
+	deg := float32(math.Sqrt(float64(dx*dx + dy*dy))*360)
+	r_Mat = matrix.RotateMatrixByDrag2D(dy, dx, deg, r_Mat)
+	view_3d.RemoveAll()
+	object.DrawObject(view_3d, object.RotateObject(O, r_Mat))
+	view_3d.Refresh()
+	d.Refresh()
+}
+
+func (d *draggableRect) DragEnd() {
+	d.Move(fyne.NewPos(0,0))
+	d.Refresh()
+}
 
 func main() {
-	r_Mat := matrix.RotationMatrix_X(matrix.DegreeToRad(180))
-	r_Mat = matrix.MultiplyMatrix(matrix.RotationMatrix_Y(matrix.DegreeToRad(240)), r_Mat)
-	r_Mat = matrix.MultiplyMatrix(matrix.RotationMatrix_X(matrix.DegreeToRad(-10)), r_Mat)
+
 	a := app.New()
 	w := a.NewWindow("3D Model Viewer")
 	w.Resize(fyne.NewSize(800, 800))
-	content := container.NewWithoutLayout()
+	content := container.NewWithoutLayout(view_3d)
+	label := widget.NewLabel("HEY")
+	label.Resize(fyne.NewSize(800,800))
+	d_rect := &draggableRect{label}
+	reset_button := widget.NewButton("Reset", func() {
+		r_Mat = matrix.RotationMatrix_X(matrix.DegreeToRad(180))
+		view_3d.RemoveAll()
+		object.DrawObject(view_3d, object.RotateObject(O, r_Mat))
+		view_3d.Refresh()
+	})
+	reset_button.Resize(fyne.NewSize(100,30))
+	content.Add(d_rect)
+	content.Add(reset_button)
 	w.SetContent(content)
 
-    O := object.ParseObject("../data/cow.obj")
-    object.DrawObject(content, object.RotateObject(O, r_Mat))
+    
+    object.DrawObject(view_3d, object.RotateObject(O, r_Mat))
 
 	w.ShowAndRun()
 }
--- src/matrix/rotation.go
+++ src/matrix/rotation.go
@@ -35,3 +35,25 @@ func RotationMatrix_Z(rad float32) (*Matrix) {
 	m.Buffer[2][2] = 1
 	return m
 }
+
+func RotationMatrixOnAxis(ux, uy float64, rad float32) (*Matrix) {
+	m := NewMatrix(3,3)
+	m.Buffer[0][0] = float32(ux*ux*(1 - math.Cos(float64(rad))) + math.Cos(float64(rad)))
+	m.Buffer[0][1] = float32(ux*uy*(1 - math.Cos(float64(rad))))
+	m.Buffer[0][2] = float32(uy*math.Sin(float64(rad)))
+	m.Buffer[1][0] = float32(ux*uy*(1 - math.Cos(float64(rad))))
+	m.Buffer[1][1] = float32(uy*uy*(1 - math.Cos(float64(rad))) + math.Cos(float64(rad)))
+	m.Buffer[1][2] = float32(ux*math.Sin(float64(rad)))*-1
+	m.Buffer[2][0] = float32(uy*math.Sin(float64(rad)))*-1
+	m.Buffer[2][1] = float32(ux*math.Sin(float64(rad)))
+	m.Buffer[2][2] = float32(math.Cos(float64(rad)))
+	return m
+}
+
+func RotateMatrixByDrag2D(dx, dy float64, deg float32, r_Mat *Matrix) (*Matrix) {
+	vec_mag := math.Sqrt(dx*dx + dy*dy)
+	ux := dx/vec_mag
+	uy := dy/vec_mag
+	rad := DegreeToRad(deg)
+	return MultiplyMatrix(RotationMatrixOnAxis(ux,uy,rad), r_Mat)
+}
