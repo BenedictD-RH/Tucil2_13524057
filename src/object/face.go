6969f6121a27be15f78c34763d33a5e179e38d76
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-03-24 23:27:39 +0700

    refactor: removed comments

--- src/object/face.go
+++ src/object/face.go
@@ -15,12 +15,8 @@ func NewFace(i1, i2, i3 int) *Face {
 }
 
 func DrawFace(content *fyne.Container, O *Object, idx int) {
-	// v1 := O.vertexArray[O.faceArray[idx].v1_idx - 1]
-	// v2 := O.vertexArray[O.faceArray[idx].v2_idx - 1]
-	// v3 := O.vertexArray[O.faceArray[idx].v3_idx - 1]
 	DrawEdgeTo2DSpace(content, O.vertexArray[O.faceArray[idx].v1_idx-1], O.vertexArray[O.faceArray[idx].v2_idx-1])
 	DrawEdgeTo2DSpace(content, O.vertexArray[O.faceArray[idx].v2_idx-1], O.vertexArray[O.faceArray[idx].v3_idx-1])
 	DrawEdgeTo2DSpace(content, O.vertexArray[O.faceArray[idx].v1_idx-1], O.vertexArray[O.faceArray[idx].v3_idx-1])
-	// face := canvas.NewArbitraryPolygon([]fyne.Position{VertexToFynePos(v1), VertexToFynePos(v2), VertexToFynePos(v3)}, color.White)
-	// content.Add(face)
+
 }
