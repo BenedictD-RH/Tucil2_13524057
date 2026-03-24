4b4045006c112e8aa76d07a42d3edc0d2424dc2f
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-03-24 13:13:05 +0700

    feat: optimized edge rendering

--- src/object/object.go
+++ src/object/object.go
@@ -1,29 +1,24 @@
 package object
 
 import (
-	"fmt"
-	"fyne.io/fyne/v2"
-	"os"
 	"bufio"
-	"log"
+	"fmt"
 	"go_project/matrix"
+	"log"
+	"math"
+	"os"
+
+	//"image/color"
+	"fyne.io/fyne/v2"
+	//"fyne.io/fyne/v2/canvas"
 )
 
 const viewbox_w = 600
 const viewbox_h = 600
-var max_v float32 = 5
-var min_v float32 = -5
 
+var Max_v float32 = -math.MaxFloat32
+var Min_v float32 = math.MaxFloat32
 
-type Face struct {
-	v1_idx int
-	v2_idx int
-	v3_idx int
-}
-
-func NewFace(i1, i2, i3 int) *Face {
-	return &Face{i1, i2, i3}
-}
 
 func (f Face) String() string {
 	return fmt.Sprintf("(%d, %d, %d)", f.v1_idx, f.v2_idx, f.v3_idx)
@@ -31,25 +26,31 @@ func (f Face) String() string {
 
 type Object struct {
 	vertexArray []*Vertex
-	faceArray []*Face
+	faceArray   []*Face
+	edgeArray   []*Edge
 }
 
 func NewObject() *Object {
-	return &Object{[]*Vertex{}, []*Face{}}
+	return &Object{[]*Vertex{}, []*Face{}, []*Edge{}}
 }
 
 func AddVertex(O *Object, v *Vertex) {
-	if (max(v.x, v.y) > max_v) {
-		max_v = max(v.x, v.y)
+	if max(v.x, v.y) > Max_v {
+		Max_v = max(v.x, v.y)
 	}
-	if (min(v.x, v.y) < min_v) {
-		min_v = min(v.x, v.y)
+	if min(v.x, v.y) < Min_v {
+		Min_v = min(v.x, v.y)
 	}
 	(*O).vertexArray = append((*O).vertexArray, v)
 }
 
 func AddFace(O *Object, f *Face) {
 	(*O).faceArray = append((*O).faceArray, f)
+	for _, edge := range getEdgeList(f) {
+		if !isEdgeInObject(O, edge) {
+			(*O).edgeArray = append((*O).edgeArray, edge)
+		}
+	}
 }
 
 func PrintObject(O *Object) {
@@ -62,25 +63,20 @@ func PrintObject(O *Object) {
 	}
 }
 
-func DrawFace(content *fyne.Container, O* Object, idx int) {
-	DrawEdge(content, O.vertexArray[O.faceArray[idx].v1_idx - 1], O.vertexArray[O.faceArray[idx].v2_idx - 1])
-	DrawEdge(content, O.vertexArray[O.faceArray[idx].v2_idx - 1], O.vertexArray[O.faceArray[idx].v3_idx - 1])
-	DrawEdge(content, O.vertexArray[O.faceArray[idx].v1_idx - 1], O.vertexArray[O.faceArray[idx].v3_idx - 1])
-}
+
 
 func DrawObject(content *fyne.Container, O *Object) {
-	for _, vert := range O.vertexArray {
-		DrawVertex(content, vert)
-	}
-
-	for i, _ := range O.faceArray {
-		DrawFace(content, O, i)
+	for i, _ := range O.edgeArray {
+		DrawEdge(content, O, i)
 	}
 }
 
-func RotateObject(O *Object, r_Mat *matrix.Matrix) (*Object) {
+func RotateObject(O *Object, r_Mat *matrix.Matrix) *Object {
 	r_O := NewObject()
 	r_O.faceArray = O.faceArray
+	r_O.edgeArray = O.edgeArray
+	Max_v = -math.MaxFloat32
+	Min_v = math.MaxFloat32
 	for _, vert := range O.vertexArray {
 		AddVertex(r_O, RotateVertex(vert, r_Mat))
 	}
@@ -101,14 +97,14 @@ func ParseObject(filename string) *Object {
 		if len(line) >= 2 {
 			identifier := line[0:2]
 			values := line[2:]
-			if (identifier == "v ") {
+			if identifier == "v " {
 				var x, y, z float32
 				fmt.Sscanf(values, "%f %f %f", &x, &y, &z)
-				AddVertex(O, NewVertex(x,y,z))
-			} else if (identifier == "f ") {
+				AddVertex(O, NewVertex(x, y, z))
+			} else if identifier == "f " {
 				var i1, i2, i3 int
 				fmt.Sscanf(values, "%d %d %d", &i1, &i2, &i3)
-				AddFace(O, NewFace(i1,i2,i3))
+				AddFace(O, NewFace(i1, i2, i3))
 			}
 		}
 	}
