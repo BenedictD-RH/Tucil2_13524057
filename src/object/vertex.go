4f293b1090ea43978d21277b9b81f79e508884bd
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-03-24 13:14:24 +0700

    refactor: moved DrawEdge to edge.go

--- src/object/vertex.go
+++ src/object/vertex.go
@@ -3,6 +3,7 @@ package object
 import (
 	"fmt"
 	"go_project/matrix"
+
 	"fyne.io/fyne/v2"
 )
 
@@ -20,46 +21,37 @@ func (v Vertex) String() string {
 	return fmt.Sprintf("(%.6f, %.6f, %.6f)", v.x, v.y, v.z)
 }
 
-func VerticeTo2DPoint(v *Vertex) *Point {
-	return &Point{(v.x - min_v)/(max_v - min_v)*viewbox_w + 100, (v.y - min_v)/(max_v - min_v)*viewbox_h + 100}
+func VertexTo2DPoint(v *Vertex) *Point {
+	return &Point{(v.x-Min_v)/(Max_v-Min_v)*viewbox_w + 100, (v.y-Min_v)/(Max_v-Min_v)*viewbox_h + 100}
+}
+
+func VertexToFynePos(v *Vertex) fyne.Position {
+	v2d := VertexTo2DPoint(v)
+	return fyne.NewPos(v2d.x, v2d.y)
 }
 
 func DrawVertex(c *fyne.Container, v *Vertex) {
-	DrawPoint(c, VerticeTo2DPoint(v))
+	DrawPoint(c, VertexTo2DPoint(v))
 }
 
-func DrawEdge(c *fyne.Container, v1 *Vertex, v2 *Vertex) {
-	DrawLine(c, &Line{VerticeTo2DPoint(v1), VerticeTo2DPoint(v2)})
-}
 
-func VertexToMatrix(v *Vertex) (*matrix.Matrix) {
-	m := matrix.NewMatrix(3,1)
+
+func VertexToMatrix(v *Vertex) *matrix.Matrix {
+	m := matrix.NewMatrix(3, 1)
 	m.Buffer[0][0] = v.x
 	m.Buffer[1][0] = v.y
 	m.Buffer[2][0] = v.z
 	return m
 }
 
-func MatrixToVertex(m *matrix.Matrix) (*Vertex) {
-	var x,y,z float32
+func MatrixToVertex(m *matrix.Matrix) *Vertex {
+	var x, y, z float32
 	x = m.Buffer[0][0]
 	y = m.Buffer[1][0]
 	z = m.Buffer[2][0]
-	return &Vertex{x,y,z}
+	return &Vertex{x, y, z}
 }
 
-func RotateVertex(v *Vertex, r_Mat *matrix.Matrix) (*Vertex) {
+func RotateVertex(v *Vertex, r_Mat *matrix.Matrix) *Vertex {
 	return MatrixToVertex(matrix.MultiplyMatrix(r_Mat, VertexToMatrix(v)))
-}
- 
-func ScanObject(content *fyne.Container) {
-	var x1, y1, z1 float32
-	var objectType string
-	fmt.Scanf("%s %f %f %f", &objectType, &x1, &y1, &z1)
-	if objectType == "v" {
-		DrawVertex(content, &Vertex{x1, y1, z1})
-		content.Refresh()
-	} else {
-		fmt.Println("Invalid")
-	}
 }
