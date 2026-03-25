67a61021cc69d2a7161d2f2f342a9e667cf7e322
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-03-25 23:56:55 +0700

    feat: 3d rendering now uses rasterization for further optimization

---
+++ src/object/raster.go
@@ -0,0 +1,100 @@
+package object
+
+import (
+	//"fmt"
+	// "runtime"
+	//"sync"
+	"image/color"
+)
+
+func isPointInSet(p Point, s[]Point) (bool) {
+	for _, p_s := range s {
+		if (int(p_s.x) == int(p.x) && int(p_s.y) == int(p.y)) {
+			return true
+		}
+	}
+	return false
+}
+
+func GetRender2DVertices(OR *ObjectRender) []Point {
+	p := []Point{}
+	for _, vert := range OR.vertices {
+		if (!isPointInSet(*VertexTo2DPoint(vert), p)) {
+			p = append(p, *VertexTo2DPoint(vert))
+		}
+	}
+	return MergeSortPointsX(p)
+}
+
+func isPixelInConvexHull(x, y int, ch []Point) bool {
+	n_vert := len(ch)
+	x_calc, y_calc := float32(x), float32(y)
+	inside := false
+	p1 := ch[0]
+	for i := 1; i < n_vert+1; i++ {
+		p2 := ch[i%n_vert]
+		if y_calc > min(p1.y, p2.y) {
+			if y_calc < max(p1.y, p2.y) {
+				if x_calc < max(p1.x, p2.x) {
+					x_intersection := (y_calc-p1.y)*(p2.x-p1.x)/(p2.y-p1.y) + p1.x
+					if p1.x == p2.x || x_calc <= x_intersection {
+						inside = !inside
+					}
+				}
+			}
+		}
+		p1 = p2
+	}
+	return inside
+}
+
+func isPixelInPolygon(x, y int, OR *ObjectRender, p_idx int) bool {
+	p := OR.polygons[p_idx]
+	x_, y_ := float32(x), float32(y)
+	v_a := VertexToFynePos(OR.vertices[p.face.v1_idx-1])
+	v_b := VertexToFynePos(OR.vertices[p.face.v2_idx-1])
+	v_c := VertexToFynePos(OR.vertices[p.face.v3_idx-1])
+	denom := ((v_b.Y-v_c.Y)*(v_a.X-v_c.X) + (v_c.X-v_b.X)*(v_a.Y-v_c.Y))
+	a := ((v_b.Y-v_c.Y)*(x_-v_c.X) + (v_c.X-v_b.X)*(y_-v_c.Y)) / denom
+	b := ((v_c.Y-v_a.Y)*(x_-v_c.X) + (v_a.X-v_c.X)*(y_-v_c.Y)) / denom
+	c := 1 - a - b
+	if a >= 0 && b >= 0 && c >= 0 {
+		return true
+	}
+	return false
+}
+
+func GetPixelColor(x, y int, OR *ObjectRender) color.Color {
+	if !isPixelInConvexHull(x, y, OR.ConvexHull) {
+		return color.Transparent
+	}
+	for i, _ := range OR.polygons {
+		if isPixelInPolygon(x, y, OR, len(OR.polygons)-1-i) {
+			return *OR.polygons[len(OR.polygons)-1-i].color
+		}
+	}
+	return color.Transparent
+}
+
+func InitializeRasterBuffer(OR *ObjectRender) {
+	for y, _ := range OR.rasterBuffer {
+		for x, _ := range OR.rasterBuffer[y] {
+			OR.rasterBuffer[y][x] = new(color.Color)
+			*(OR.rasterBuffer[y][x]) = color.Transparent
+		}
+	}
+}
+
+func UpdateRasterBuffer(OR *ObjectRender) {
+	start_y := (len(OR.rasterBuffer) - viewbox_h) / 2
+	end_y := len(OR.rasterBuffer) - start_y
+	start_x := (len(OR.rasterBuffer[0]) - viewbox_w) / 2
+	end_x := len(OR.rasterBuffer[0]) - start_x
+	for y := start_y; y < end_y; y++ {
+		for x := start_x; x < end_x; x++ {
+			go func() {
+				*(OR.rasterBuffer[y][x]) = GetPixelColor(x, y, OR)
+			}()
+		}
+	}
+}
