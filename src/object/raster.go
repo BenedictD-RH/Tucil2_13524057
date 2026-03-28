9985bcad18fc1749b0c46a5fc3e1e99ed69d603e
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-03-28 16:55:09 +0700

    feat: increased render accuracy

--- src/object/raster.go
+++ src/object/raster.go
@@ -1,15 +1,16 @@
 package object
 
 import (
-	//"fmt"
+	"math"
 	// "runtime"
 	//"sync"
 	"image/color"
 )
 
-func isPointInSet(p Point, s[]Point) (bool) {
+func isPointInSet(p Point, s []Point) bool {
 	for _, p_s := range s {
-		if (int(p_s.x) == int(p.x) && int(p_s.y) == int(p.y)) {
+		if (math.Round(float64(p_s.x)) == math.Round(float64(p.x))) && (math.Round(float64(p_s.y)) == math.Round(float64(p.y))) {
+			
 			return true
 		}
 	}
@@ -18,12 +19,28 @@ func isPointInSet(p Point, s[]Point) (bool) {
 
 func GetRender2DVertices(OR *ObjectRender) []Point {
 	p := []Point{}
-	for _, vert := range OR.vertices {
-		if (!isPointInSet(*VertexTo2DPoint(vert), p)) {
+	for _, vert := range OR.Vertices {
+		if !isPointInSet(*VertexTo2DPoint(vert), p) {
 			p = append(p, *VertexTo2DPoint(vert))
 		}
 	}
 	return MergeSortPointsX(p)
+}
+
+func PixelZValue(x, y int, OR *ObjectRender, p_idx int) float32 {
+	p := OR.Polygons[p_idx]
+	x_, y_ := float32(x), float32(y)
+	x1, y1 := VertexToFynePos(OR.Vertices[p.face.v1_idx-1]).X, VertexToFynePos(OR.Vertices[p.face.v1_idx-1]).Y
+	z1 := OR.Vertices[p.face.v1_idx-1].z
+	x2, y2 := VertexToFynePos(OR.Vertices[p.face.v2_idx-1]).X, VertexToFynePos(OR.Vertices[p.face.v2_idx-1]).Y
+	z2 := OR.Vertices[p.face.v2_idx-1].z
+	x3, y3 := VertexToFynePos(OR.Vertices[p.face.v3_idx-1]).X, VertexToFynePos(OR.Vertices[p.face.v3_idx-1]).Y
+	z3 := OR.Vertices[p.face.v3_idx-1].z
+	A := y1*(z2-z3) + y2*(z3-z1) + y3*(z1-z2)
+	B := z1*(x2-x3) + z2*(x3-x1) + z3*(x1-x2)
+	C := x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2)
+	D := -x1*(y2*z3-y3*z2) - x2*(y3*z1-y1*z3) - x3*(y1*z2-y2*z1)
+	return (-D - A*x_ - B*y_) / C
 }
 
 func isPixelInConvexHull(x, y int, ch []Point) bool {
@@ -49,11 +66,11 @@ func isPixelInConvexHull(x, y int, ch []Point) bool {
 }
 
 func isPixelInPolygon(x, y int, OR *ObjectRender, p_idx int) bool {
-	p := OR.polygons[p_idx]
+	p := OR.Polygons[p_idx]
 	x_, y_ := float32(x), float32(y)
-	v_a := VertexToFynePos(OR.vertices[p.face.v1_idx-1])
-	v_b := VertexToFynePos(OR.vertices[p.face.v2_idx-1])
-	v_c := VertexToFynePos(OR.vertices[p.face.v3_idx-1])
+	v_a := VertexToFynePos(OR.Vertices[p.face.v1_idx-1])
+	v_b := VertexToFynePos(OR.Vertices[p.face.v2_idx-1])
+	v_c := VertexToFynePos(OR.Vertices[p.face.v3_idx-1])
 	denom := ((v_b.Y-v_c.Y)*(v_a.X-v_c.X) + (v_c.X-v_b.X)*(v_a.Y-v_c.Y))
 	a := ((v_b.Y-v_c.Y)*(x_-v_c.X) + (v_c.X-v_b.X)*(y_-v_c.Y)) / denom
 	b := ((v_c.Y-v_a.Y)*(x_-v_c.X) + (v_a.X-v_c.X)*(y_-v_c.Y)) / denom
@@ -68,9 +85,32 @@ func GetPixelColor(x, y int, OR *ObjectRender) color.Color {
 	if !isPixelInConvexHull(x, y, OR.ConvexHull) {
 		return color.Transparent
 	}
-	for i, _ := range OR.polygons {
-		if isPixelInPolygon(x, y, OR, len(OR.polygons)-1-i) {
-			return *OR.polygons[len(OR.polygons)-1-i].color
+	for i, _ := range OR.Polygons {
+		r_i := len(OR.Polygons) - 1 - i
+		if isPixelInPolygon(x, y, OR, r_i) {
+			higher_exist_below := false
+			j := 1
+			n := 0
+			for {
+				if (r_i-j >= 0) && (j < len(OR.Polygons)/10 || len(OR.Polygons) < 20) {
+					if isPixelInPolygon(x, y, OR, r_i-j) {
+						if PixelZValue(x, y, OR, r_i) < PixelZValue(x, y, OR, r_i-j) {
+							higher_exist_below = true
+							break
+						}
+						n++
+					}
+				} else {
+					break
+				}
+				j++
+				if n >= 10 {
+					break
+				}
+			}
+			if !higher_exist_below {
+				return *OR.Polygons[r_i].color
+			}
 		}
 	}
 	return color.Transparent
