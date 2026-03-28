4cc2d0d5986774b8cf33ee8a82adcee449a4de3e
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-03-28 22:40:11 +0700

    feat: added octree measurement functions

--- src/voxel/octree.go
+++ src/voxel/octree.go
@@ -1,52 +1,91 @@
 package voxel
 
 import (
-	"fmt"
-	"sync"
 	"go_project/object"
+	"sync"
 )
 
 type Octree struct {
-	value bool
-	object *object.Object
-	voxel Voxel
+	value             bool
+	object            *object.Object
+	voxel             Voxel
 	intersectingFaces []int
-	children [8]*Octree
+	children          [8]*Octree
 }
 
-func NewOctreeRoot(O *object.Object) (*Octree) {
+func NewOctreeRoot(O *object.Object) *Octree {
 	face_idx_list := []int{}
 	for i := range O.FaceArray {
 		face_idx_list = append(face_idx_list, i)
 	}
-	voxel := Voxel{*object.NewVertex(0,0,0), maxEdgeLength}
+	voxel := Voxel{*object.NewVertex(0, 0, 0), MaxEdgeLength}
 	return &Octree{true,
-				   O, 
-				   voxel,
-				   face_idx_list,
-				   [8]*Octree{nil,nil,nil,nil,nil,nil,nil,nil}}
+		O,
+		voxel,
+		face_idx_list,
+		[8]*Octree{nil, nil, nil, nil, nil, nil, nil, nil}}
 }
 
-func NewOctree(voxel Voxel, fl []int, O* object.Object) (*Octree) {
+func NewOctree(voxel Voxel, fl []int, O *object.Object) *Octree {
 	return &Octree{true,
-				   O,
-				   voxel,
-				   fl,
-				   [8]*Octree{nil,nil,nil,nil,nil,nil,nil,nil}}
+		O,
+		voxel,
+		fl,
+		[8]*Octree{nil, nil, nil, nil, nil, nil, nil, nil}}
 }
 
-func isLeaf(Oct *Octree) (bool) {
+func isLeaf(Oct *Octree) bool {
 	return !Oct.value || (Oct.children[0] == nil)
 }
 
-func getTreeDepth(Oct *Octree) (int) {
+func amountOfEmptyNodesAtDepth(Oct *Octree, n, depth int) int {
+	if Oct == nil {
+		return 0
+	} else if n == depth {
+		if !Oct.value {
+			return 1
+		} else {
+			return 0
+		}
+	} else {
+		total := 0
+		for _, c := range Oct.children {
+			total += amountOfEmptyNodesAtDepth(c, n+1, depth)
+		}
+		return total
+	}
+}
+
+func amountOfNodeAtDepth(Oct *Octree, n, depth int) int {
+	if Oct == nil {
+		return 0
+	} else if n == depth {
+		return 1
+	} else {
+		total := 0
+		for _, c := range Oct.children {
+			total += amountOfNodeAtDepth(c, n+1, depth)
+		}
+		return total
+	}
+}
+
+func GetAmountOfNodeAtDepth(Oct *Octree, depth int) int {
+	return amountOfNodeAtDepth(Oct, 0, depth)
+}
+
+func GetAmountOfEmptyNodesAtDepth(Oct *Octree, depth int) int {
+	return amountOfEmptyNodesAtDepth(Oct, 0, depth)
+}
+
+func GetTreeMaxDepth(Oct *Octree) int {
 	if isLeaf(Oct) {
 		return 1
 	} else {
 		maxDepth := 0
 		for _, p := range Oct.children {
-			p_depth := 1 + getTreeDepth(p)
-			if  p_depth > maxDepth {
+			p_depth := 1 + GetTreeMaxDepth(p)
+			if p_depth > maxDepth {
 				maxDepth = p_depth
 			}
 		}
@@ -54,14 +93,14 @@ func getTreeDepth(Oct *Octree) (int) {
 	}
 }
 
-func getOctreeVoxelEdgeLength(Oct *Octree) (float32) {
+func getOctreeVoxelEdgeLength(Oct *Octree) float32 {
 	if isLeaf(Oct) {
 		return Oct.voxel.edgeLength
 	} else {
-		minEdgeLength := maxEdgeLength
+		minEdgeLength := MaxEdgeLength
 		for _, p := range Oct.children {
 			p_el := getOctreeVoxelEdgeLength(p)
-			if  p_el < minEdgeLength {
+			if p_el < minEdgeLength {
 				minEdgeLength = p_el
 			}
 		}
@@ -73,10 +112,10 @@ func IncreaseDepth(Oct *Octree) {
 	if isLeaf(Oct) && Oct.value {
 		voxelCorners := GetVoxelVertices(&Oct.voxel)
 		for i := range Oct.children {
-			subVox := Voxel{*object.MidVertex(&Oct.voxel.center, voxelCorners[i]), Oct.voxel.edgeLength/2}
+			subVox := Voxel{*object.MidVertex(&Oct.voxel.center, voxelCorners[i]), Oct.voxel.edgeLength / 2}
 			new_i_f := []int{}
 			for _, f_idx := range Oct.intersectingFaces {
-				if (isPolygonClippingVoxel(subVox, Oct.object, f_idx)) {
+				if isPolygonClippingVoxel(subVox, Oct.object, f_idx) {
 					new_i_f = append(new_i_f, f_idx)
 				}
 			}
@@ -96,7 +135,21 @@ func IncreaseDepth(Oct *Octree) {
 	}
 }
 
-func OctreeToVoxelObject(Oct *Octree) (*VoxelObject) {
+func OctreeVoxelAmount(Oct *Octree) int {
+	if !Oct.value {
+		return 0
+	} else if isLeaf(Oct) {
+		return 1
+	} else {
+		total := 0
+		for _, o := range Oct.children {
+			total += OctreeVoxelAmount(o)
+		}
+		return total
+	}
+}
+
+func OctreeToVoxelObject(Oct *Octree) *VoxelObject {
 	if !Oct.value {
 		return &VoxelObject{[]*object.Vertex{}, []*object.Face{}}
 	} else if isLeaf(Oct) {
@@ -110,10 +163,3 @@ func OctreeToVoxelObject(Oct *Octree) (*VoxelObject) {
 		return O
 	}
 }
-
-func OctreeTest(O *object.Object) {
-	Oct := NewOctreeRoot(NormalizeObject(O))
-	fmt.Println(getOctreeVoxelEdgeLength(Oct))
-}
-
-	
